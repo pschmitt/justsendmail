a786848003fee21a774137ffbbb6f1037ddb5f94
Philipp Schmitt <philipp@schmitt.co>
2020-07-31 10:59:06 +0200

    Black.

--- setup.py
+++ setup.py
@@ -8,12 +8,10 @@ setup(
     description="Simple lib to send mail",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    entry_points={
-        'console_scripts': ['justsendmail=justsendmail.mail:main'],
-    },
+    entry_points={"console_scripts": ["justsendmail=justsendmail.mail:main"],},
     author="Philipp Schmitt",
     author_email="philipp@schmitt.co",
     url="https://github.com/pschmitt/pymail_utils",
     packages=find_packages(),
-    include_package_data=True
+    include_package_data=True,
 )
